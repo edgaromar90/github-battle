a24f3446c029efebcb5fe50f4caab163bde88e85
Edgar Henriquez <edgaromar90@gmail.com>
2019-10-03 18:50:16 -0400

    add reset battle functionality

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -146,6 +146,11 @@ export default class Battle extends React.Component {
         <Results
           playerOne={playerOne}
           playerTwo={playerTwo}
+          onReset={() => this.setState({
+            playerOne: null,
+            playerTwo: null,
+            battle: false,
+          })}
          />
       )
     }
