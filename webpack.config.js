8f77b523f94143f22103230491fdc2a687872576
Edgar Henriquez <edgaromar90@gmail.com>
2019-10-07 20:12:50 -0400

    adding redirects to host with Netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/index.js',
@@ -18,7 +19,10 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: '_redirects' }
+    ])
   ],
   devServer: {
     historyApiFallback: true
